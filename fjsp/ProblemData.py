34261b5396362cdfcdc89c77770474392e91316d
Leon Lan <l.lan@vu.nl>
2023-11-26 14:08:23 +0100

    Fix test
    Good case for checking against is None instead of bool

--- fjsp/ProblemData.py
+++ fjsp/ProblemData.py
@@ -72,10 +72,18 @@ class Operation:
         latest_end: Optional[int] = None,
         name: Optional[str] = None,
     ):
-        if earliest_start and latest_start and earliest_start > latest_start:
+        if (
+            earliest_start is not None
+            and latest_start is not None
+            and earliest_start > latest_start
+        ):
             raise ValueError("earliest_start must be <= latest_start.")
 
-        if earliest_end and latest_end and earliest_end > latest_end:
+        if (
+            earliest_end is not None
+            and latest_end is not None
+            and earliest_end > latest_end
+        ):
             raise ValueError("earliest_end must be <= latest_end.")
 
         self._earliest_start = earliest_start
