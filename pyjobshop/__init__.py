9948fd4e03896b13e84e47a07653f7d4ede1d603
Leon Lan <l.lan@vu.nl>
2024-03-22 17:13:31 +0100

    Imports

--- pyjobshop/__init__.py
+++ pyjobshop/__init__.py
@@ -1,10 +1,9 @@
-from .cp import default_model, result2solution
-from .Model import Model
-from .plot import plot
-from .ProblemData import (
-    Job,
-    Machine,
-    Operation,
-    Precedence,
-    ProblemData,
-)
+from .cp import default_model as default_model
+from .cp import result2solution as result2solution
+from .Model import Model as Model
+from .plot import plot as plot
+from .ProblemData import Job as Job
+from .ProblemData import Machine as Machine
+from .ProblemData import Operation as Operation
+from .ProblemData import Precedence as Precedence
+from .ProblemData import ProblemData as ProblemData
