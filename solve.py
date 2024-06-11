e046a4ad9882c75786c011e353a9aada754fa9b6
Leon Lan <l.lan@vu.nl>
2024-06-11 16:22:53 +0200

    Argparser

--- solve.py
+++ solve.py
@@ -1,22 +1,54 @@
+import argparse
+from pathlib import Path
+
 import fjsplib
 from fjsplib import read
 
 import pyjobshop
-from pyjobshop import Constraint, Model
+from pyjobshop import Constraint, Model, solve
 
 
-def instance2model(instance: fjsplib.Instance) -> pyjobshop.Model:
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    msg = "Location of the instance file."
+    parser.add_argument("instance_loc", type=Path, help=msg)
+
+    msg = "Solver to use."
+    parser.add_argument(
+        "--solver",
+        type=str,
+        default="ortools",
+        choices=["ortools", "cpoptimizer"],
+        help=msg,
+    )
+
+    msg = "Time limit for solving the instance, in seconds."
+    parser.add_argument("--time_limit", type=float, default=10, help=msg)
+
+    msg = "Whether to log the solver output."
+    parser.add_argument("--log", action="store_true", help=msg)
+
+    msg = "Number of workers to use for parallel solving."
+    parser.add_argument("--num_workers", type=int, default=8, help=msg)
+
+    return parser.parse_args()
+
+
+def instance2data(instance: fjsplib.Instance) -> pyjobshop.ProblemData:
+    """
+    Converts an FJSPLIB instance to a ProblemData object.
+    """
     m = Model()
 
     jobs = [m.add_job() for _ in range(instance.num_jobs)]
     machines = [m.add_machine() for _ in range(instance.num_machines)]
 
     for job_idx, tasks in enumerate(instance.jobs):
-        for task_ in tasks:
+        for task_data in tasks:
             task = m.add_task(job=jobs[job_idx])
 
-            for machine_idx, duration in task_:
-                print(machine_idx, duration)
+            for machine_idx, duration in task_data:
                 m.add_processing_time(machines[machine_idx], task, duration)
 
     for frm, to in instance.precedences:
@@ -26,9 +58,21 @@ def instance2model(instance: fjsplib.Instance) -> pyjobshop.Model:
             Constraint.END_BEFORE_START,
         )
 
-    return m
+    return m.data()
 
 
-instance = read("instances/FJSP/Barnes/seti5xyz.fjs")
-m = instance2model(instance)
-m.solve(time_limit=10)
+def main(
+    instance_loc: Path,
+    solver: str,
+    time_limit: float,
+    log: bool,
+    num_workers: int,
+):
+    instance = read(instance_loc)
+    data = instance2data(instance)
+    result = solve(data, solver, time_limit, log, num_workers)
+    print(result)
+
+
+if __name__ == "__main__":
+    main(**vars(parse_args()))
