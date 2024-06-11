0202d86fbe4c6ed9cd580ee30f48011a3080960d
Leon Lan <l.lan@vu.nl>
2024-06-11 19:35:27 +0200

    Batch solve

--- pyjobshop/cli.py
+++ pyjobshop/cli.py
@@ -2,7 +2,7 @@ import argparse
 from pathlib import Path
 
 import fjsplib
-from fjsplib import read
+import numpy as np
 
 import pyjobshop
 from pyjobshop import Constraint, Model, solve
@@ -12,7 +12,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     msg = "Location of the instance file."
-    parser.add_argument("instance_loc", type=Path, help=msg)
+    parser.add_argument("instances", nargs="+", type=Path, help=msg)
 
     msg = "Solver to use."
     parser.add_argument(
@@ -33,6 +33,29 @@ def parse_args():
     parser.add_argument("--num_workers", type=int, default=8, help=msg)
 
     return parser.parse_args()
+
+
+def tabulate(headers: list[str], rows: np.ndarray) -> str:
+    """
+    Creates a simple table from the given header and row data.
+    """
+    # These lengths are used to space each column properly.
+    lens = [len(header) for header in headers]
+
+    for row in rows:
+        for idx, cell in enumerate(row):
+            lens[idx] = max(lens[idx], len(str(cell)))
+
+    header = [
+        "  ".join(f"{hdr:<{ln}s}" for ln, hdr in zip(lens, headers)),
+        "  ".join("-" * ln for ln in lens),
+    ]
+
+    content = [
+        "  ".join(f"{c!s:>{ln}s}" for ln, c in zip(lens, r)) for r in rows
+    ]
+
+    return "\n".join(header + content)
 
 
 def instance2data(instance: fjsplib.Instance) -> pyjobshop.ProblemData:
@@ -58,20 +81,48 @@ def instance2data(instance: fjsplib.Instance) -> pyjobshop.ProblemData:
             Constraint.END_BEFORE_START,
         )
 
+    m.set_planning_horizon(4500)
     return m.data()
 
 
-def benchmark(
+def _solve(
     instance_loc: Path,
     solver: str,
     time_limit: float,
     log: bool,
     num_workers: int,
-):
-    instance = read(instance_loc)
+) -> tuple[str, bool, float, float]:
+    """
+    Solves a single instance.
+    """
+    instance = fjsplib.read(instance_loc)
     data = instance2data(instance)
     result = solve(data, solver, time_limit, log, num_workers)
-    print(result)
+
+    return (
+        instance_loc.name,
+        result.status.value in ["Optimal", "Feasible"],
+        result.objective,
+        round(result.runtime, 2),
+    )
+
+
+def benchmark(instances: list[Path], **kwargs):
+    results = [_solve(instance, **kwargs) for instance in instances]
+    dtypes = [
+        ("inst", "U37"),
+        ("feas", "U1"),
+        ("obj", float),
+        ("time", float),
+    ]
+
+    data = np.asarray(results, dtype=dtypes)
+    headers = ["Instance", "Feas.", "Obj.", "Time (s)"]
+
+    print("\n", tabulate(headers, data), "\n", sep="")
+    print(f"     Avg. objective: {data['obj'].mean():.0f}")
+    print(f"      Avg. run-time: {data['time'].mean():.2f}s")
+    print(f"       Total infeas: {np.count_nonzero(data['feas'])}")
 
 
 def main():
