6759aaa773083c4bb190ccf84c80be69e7b981eb
Leon Lan <30997278+leonlan@users.noreply.github.com>
2024-06-17 14:36:56 +0200

    Pass additional solver parameters to solve (#149)
    * Pass kwargs to solve
    
    * Test solve
    
    * Add relevant issue number to TODOs

--- pyjobshop/solve.py
+++ pyjobshop/solve.py
@@ -14,10 +14,11 @@ except ModuleNotFoundError:
 
 def solve(
     data: ProblemData,
-    solver: str,
+    solver: str = "ortools",
     time_limit: float = float("inf"),
     log: bool = False,
     num_workers: Optional[int] = None,
+    **kwargs,
 ) -> Result:
     """
     Solves the given problem data instance.
@@ -27,36 +28,43 @@ def solve(
     data
         The problem data instance.
     solver
-        The solver to use. Either "ortools" or "cpoptimizer".
+        The solver to use. Either ``ortools`` or ``cpoptimizer``.
     time_limit
         The time limit for the solver in seconds. Default ``float('inf')``.
     log
         Whether to log the solver output. Default ``False``.
     num_workers
-        The number of workers to use for parallel solving. If not
-        specified, the default value of the selected solver is used, which
-        is typically the maximum number of available CPU cores.
+        The number of workers to use for parallel solving. If not specified,
+        the default of the selected solver is used, which is typically the
+        number of available CPU cores.
+    kwargs
+        Additional parameters passed to the solver.
 
     Returns
     -------
     Result
         A Result object containing the best found solution and additional
         information about the solver run.
+
+    Raises
+    ------
+    ModuleNotFoundError
+        If CP Optimizer is chosen but its dependencies are not installed.
     """
-    if solver not in ["ortools", "cpoptimizer"]:
+    if solver == "ortools":
+        return pyjobshop.ortools.solve(
+            data, time_limit, log, num_workers, **kwargs
+        )
+    elif solver == "cpoptimizer" and CPOPTIMIZER_AVAILABLE:
+        return pyjobshop.cpoptimizer.solve(
+            data, time_limit, log, num_workers, **kwargs
+        )
+    elif solver == "cpoptimizer" and not CPOPTIMIZER_AVAILABLE:
+        msg = (
+            "Using CP Optimizer requires the relevant dependencies to be "
+            "installed. You can install those using `pip install "
+            "pyjobshop[cpoptimizer]`."
+        )
+        raise ModuleNotFoundError(msg)
+    else:
         raise ValueError(f"Unknown solver choice: {solver}.")
-
-    args = data, time_limit, log, num_workers
-
-    if solver == "cpoptimizer":
-        if not CPOPTIMIZER_AVAILABLE:
-            msg = (
-                "Using CP Optimizer requires the relevant dependencies to be "
-                "installed. You can install those using `pip install "
-                "pyjobshop[cpoptimizer]`."
-            )
-            raise ModuleNotFoundError(msg)
-
-        return pyjobshop.cpoptimizer.solve(*args)
-
-    return pyjobshop.ortools.solve(*args)
