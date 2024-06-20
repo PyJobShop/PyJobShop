41d60964006e4848802222eb313b03772e20377f
Leon Lan <l.lan@vu.nl>
2024-06-20 16:08:54 +0200

    Reorganize tests

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,7 +1,7 @@
 import pytest
 from numpy.testing import assert_, assert_equal
 
-from pyjobshop import Model, solve
+from pyjobshop import solve
 from pyjobshop.Solution import Solution, TaskData
 
 
@@ -16,7 +16,7 @@ def test_solve(small, solver):
     assert_equal(result.objective, 3)
 
 
-def test_unknown_solver(small):
+def test_solve_unknown_solver(small):
     """
     Tests that an unknown solver raises a ValueError.
     """
@@ -24,107 +24,62 @@ def test_unknown_solver(small):
         solve(small, "unknown")
 
 
-def test_solve_initial_solution(small, capfd):
-    init = Solution([TaskData(0, 0, 1, 1), TaskData(0, 1, 2, 3)])
-    solve(
-        small,
-        "ortools",
-        log=True,
-        initial_solution=init,
-        fix_variables_to_their_hinted_value=True,
-    )
+def test_solve_log(small, solver, capfd):
+    """
+    Tests that setting the log flag correctly show solver output.
+    """
+    solve(small, solver, log=True)
     printed = capfd.readouterr().out
+    assert_(printed != "")
 
-    # Not all variables are hinted (only job/task/task_alt) so this message
-    # is correct.
-    assert_("The solution hint is incomplete" in printed)
-
-    # TODO CP Optimizer?
-
-
-def test_cp_opt(small, capfd):
-    init = Solution([TaskData(0, 0, 1, 1), TaskData(0, 1, 2, 3)])
-
-    solve(small, "cpoptimizer", log=True, initial_solution=init)
+    solve(small, solver, log=False)
     printed = capfd.readouterr().out
+    assert_equal(printed, "")
 
-    msg = "Starting point is complete and consistent with constraints."
+
+def test_solve_time_limit(small, capfd):
+    """
+    Tests the log that the time limit is set. No test for CP Optimizer
+    because it does not log this setting.
+    """
+    solve(small, "ortools", time_limit=1.2, log=True)
+    printed = capfd.readouterr().out
+    assert_("max_time_in_seconds: 1.2" in printed)
+
+
+@pytest.mark.parametrize(
+    "solver_, msg",
+    [
+        ("ortools", "num_workers: 2"),
+        ("cpoptimizer", "Using parallel search with 2 workers."),
+    ],
+)
+def test_solve_num_workers(small, solver_, msg, capfd):
+    """
+    Tests the log that the ``num_workers`` parameter is correctly set.
+    """
+    solve(small, solver_, num_workers=2, log=True)
+    printed = capfd.readouterr().out
     assert_(msg in printed)
 
 
-def test_solve_initial_solution_setup(capfd):
-    init = Solution([TaskData(0, 0, 2, 2), TaskData(0, 6, 2, 8)])
-    model = Model()
-    job = model.add_job()
-    machine = model.add_machine()
-    task1 = model.add_task(job=job)
-    task2 = model.add_task(job=job)
-
-    model.add_processing_time(task1, machine, 2)
-    model.add_processing_time(task2, machine, 2)
-    model.add_setup_time(machine, task1, task2, 2)
-    model.add_previous(task1, task2)
-
-    result = solve(
-        model.data(),
-        "ortools",
-        log=True,
-        initial_solution=init,
-        fix_variables_to_their_hinted_value=True,
-    )
+@pytest.mark.parametrize(
+    "solver_, msg",
+    [
+        # Not all variables are hinted so this message is correct.
+        ("ortools", "The solution hint is incomplete"),
+        (
+            "cpoptimizer",
+            "Starting point is complete and consistent with constraints.",
+        ),
+    ],
+)
+def test_solve_initial_solution(small, solver_, msg, capfd):
+    init = Solution([TaskData(0, 0, 1, 1), TaskData(0, 1, 2, 3)])
+    solve(small, solver_, log=True, initial_solution=init)
     printed = capfd.readouterr().out
 
-    assert_("The solution hint is incomplete" in printed)
-    assert_equal(result.objective, 8)
-
-    # Don't fix
-    result = solve(model.data(), "ortools", log=True, initial_solution=init)
-    printed = capfd.readouterr().out
-
-    assert_("The solution hint is incomplete" in printed)
-    assert_equal(result.objective, 6)
-
-
-def describe_solve_set_default_parameters():
-    """
-    Tests `solve` when setting the default parameters.
-    """
-
-    def log(small, solver, capfd):
-        """
-        Checks that setting the log flag correctly show solver output.
-        """
-        solve(small, solver, log=True)
-        printed = capfd.readouterr().out
-        assert_(printed != "")
-
-        solve(small, solver, log=False)
-        printed = capfd.readouterr().out
-        assert_equal(printed, "")
-
-    def time_limit(small, capfd):
-        """
-        Checks the log that the time limit is set. No test for CP Optimizer
-        because it does not log this setting.
-        """
-        solve(small, "ortools", time_limit=1.2, log=True)
-        printed = capfd.readouterr().out
-        assert_("max_time_in_seconds: 1.2" in printed)
-
-    @pytest.mark.parametrize(
-        "solver_, msg",
-        [
-            ("ortools", "num_workers: 2"),
-            ("cpoptimizer", "Using parallel search with 2 workers."),
-        ],
-    )
-    def num_workers(small, solver_, msg, capfd):
-        """
-        Checks the log that the ``num_workers`` parameter is correctly set.
-        """
-        solve(small, solver_, num_workers=2, log=True)
-        printed = capfd.readouterr().out
-        assert_(msg in printed)
+    assert_(msg in printed)
 
 
 @pytest.mark.parametrize(
