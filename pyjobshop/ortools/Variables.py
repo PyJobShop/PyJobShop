d0d32b3f39619e427f27329831202c16ee16e406
Leon Lan <l.lan@vu.nl>
2024-08-02 14:02:38 +0200

    Update Variables docstring

--- pyjobshop/ortools/Variables.py
+++ pyjobshop/ortools/Variables.py
@@ -149,7 +149,7 @@ class SequenceVar:
 
 class Variables:
     """
-    Helper class that creates the variables for the OR-Tools CP solver.
+    Helper class that creates the core variables for OR-Tools.
     """
 
     def __init__(self, m: CpModel, data: ProblemData):
