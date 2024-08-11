011d8b4c25d651166ea400546a112114c3be56cb
Leon Lan <l.lan@vu.nl>
2024-08-11 10:32:55 +0200

    Update tests to pass with --solvers ortools

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -47,60 +47,56 @@ def test_solve_time_limit(small, capfd):
     assert_("max_time_in_seconds: 1.2" in printed)
 
 
-@pytest.mark.parametrize(
-    "solver_, msg",
-    [
-        ("ortools", "num_workers: 2"),
-        ("cpoptimizer", "Using parallel search with 2 workers."),
-    ],
-)
-def test_solve_num_workers(small, solver_, msg, capfd):
+def test_solve_num_workers(small, solver, capfd):
     """
     Tests the log that the ``num_workers`` parameter is correctly set.
     """
-    solve(small, solver_, num_workers=2, display=True)
+    solver2msg = {
+        "ortools": "num_workers: 2",
+        "cpoptimizer": "Using parallel search with 2 workers.",
+    }
+    msg = solver2msg[solver]
+
+    solve(small, solver, num_workers=2, display=True)
     printed = capfd.readouterr().out
     assert_(msg in printed)
 
 
-@pytest.mark.parametrize(
-    "solver_, msg",
-    [
+def test_solve_initial_solution(small, solver, capfd):
+    """
+    Tests that the log message is correct when an initial solution is provided.
+    """
+    solver2msg = {
         # Not all variables are hinted so this message is correct.
-        ("ortools", "The solution hint is incomplete"),
-        (
-            "cpoptimizer",
-            "Starting point is complete and consistent with constraints.",
-        ),
-    ],
-)
-def test_solve_initial_solution(small, solver_, msg, capfd):
-    init = Solution([TaskData(0, 0, 1, 1), TaskData(0, 1, 2, 3)])
-    solve(small, solver_, display=True, initial_solution=init)
-    printed = capfd.readouterr().out
+        "ortools": "The solution hint is incomplete",
+        "cpoptimizer": "Starting point is complete and consistent with constraints.",  # noqa
+    }
+    msg = solver2msg[solver]
 
+    init = Solution([TaskData(0, 0, 1, 1), TaskData(0, 1, 2, 3)])
+    solve(small, solver, display=True, initial_solution=init)
+    printed = capfd.readouterr().out
     assert_(msg in printed)
 
 
-@pytest.mark.parametrize(
-    "solver_, param, value",
-    [
-        ("ortools", "log_search_progress", True),
-        ("cpoptimizer", "LogVerbosity", "Terse"),
-    ],
-)
-def test_solve_additional_params(small, capfd, solver_, param, value):
+def test_solve_additional_params(small, solver, capfd):
     """
     Tests the solve method with additional parameters can override the
     parameters supported by ``solve``.
     """
+    solver2param_value = {
+        "ortools": ("log_search_progress", True),
+        "cpoptimizer": ("LogVerbosity", "Terse"),
+    }
+    param, value = solver2param_value[solver]
+
     # Let's test that setting log to False will not print anything.
-    solve(small, solver_, display=False)
+    solve(small, solver, display=False)
     printed = capfd.readouterr().out
     assert_equal(printed, "")
 
     # Now we set the corresponding log parameter using the additional keyword
     # argument, which will override the earlier setting of log being False.
-    solve(small, solver_, display=False, **{param: value})
+    solve(small, solver, display=False, **{param: value})
     printed = capfd.readouterr().out
     assert_(printed != "")
