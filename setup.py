ab53d4afcc9cdeb7f74beebe3806fb0925efcb19
Leon Lan <l.lan@vu.nl>
2024-08-28 19:00:17 +0200

    Bump version for another test release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pyjobshop",
-    version="0.0.2a",
+    version="0.0.2b",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
