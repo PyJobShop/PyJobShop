b7367793f5d170da7ae7219dbb462a2a53ac35c5
Leon Lan <l.lan@vu.nl>
2024-08-28 18:58:59 +0200

    Setup.py find packages without args
    This will automagically find the package.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="pyjobshop",
     version="0.0.2a",
-    packages=find_packages(include=["pyjobshop"]),
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "pyjobshop=pyjobshop.cli:main",
