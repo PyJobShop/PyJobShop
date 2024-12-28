7d61ced04ab3e2b88d50e293f7a95c45dddcd152
Leon Lan <30997278+leonlan@users.noreply.github.com>
2024-12-28 19:26:45 +0100

    Result string representation (#264)

--- pyjobshop/Result.py
+++ pyjobshop/Result.py
@@ -35,3 +35,13 @@ class Result:
     runtime: float
     best: Solution
     objective: float
+
+    def __str__(self):
+        content = [
+            "Solution results",
+            "================",
+            f"objective: {self.objective:.2f}",
+            f"   status: {self.status.value}",
+            f"  runtime: {self.runtime:.2f} seconds",
+        ]
+        return "\n".join(content)
--- tests/test_Result.py
+++ tests/test_Result.py
@@ -15,3 +15,20 @@ def test_result_attributes():
     assert_equal(result.runtime, 123.45)
     assert_equal(result.best, solution)
     assert_equal(result.objective, 100)
+
+
+def test_result_string_representation():
+    """
+    Test that the string representation of a Result object is correct.
+    """
+    solution = Solution([])
+    result = Result(SolveStatus.OPTIMAL, 100.00001, solution, 123.45)
+    expected = (
+        "Solution results\n"
+        "================\n"
+        "objective: 123.45\n"
+        "   status: Optimal\n"
+        "  runtime: 100.00 seconds"
+    )
+
+    assert_equal(str(result), expected)
