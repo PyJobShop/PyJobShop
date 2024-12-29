a783ad1bb61943e21e1561cc066629227ce2507f
Leon Lan <l.lan@vu.nl>
2024-12-29 09:12:18 +0100

    Fix typo

--- pyjobshop/Model.py
+++ pyjobshop/Model.py
@@ -380,7 +380,9 @@ class Model:
 
         return mode
 
-    def _add_constaint(self, task1: Task, task2: Task, constraint: Constraint):
+    def _add_constraint(
+        self, task1: Task, task2: Task, constraint: Constraint
+    ):
         """
         Adds a constraint between two tasks.
         """
@@ -393,70 +395,70 @@ class Model:
         Adds a constraint that the first task must start at the same time as
         the second task starts.
         """
-        self._add_constaint(task1, task2, Constraint.START_AT_START)
+        self._add_constraint(task1, task2, Constraint.START_AT_START)
 
     def add_start_at_end(self, task1: Task, task2: Task):
         """
         Adds a constraint that the first task must start at the same time as
         the second task ends.
         """
-        self._add_constaint(task1, task2, Constraint.START_AT_END)
+        self._add_constraint(task1, task2, Constraint.START_AT_END)
 
     def add_start_before_start(self, task1: Task, task2: Task):
         """
         Adds a constraint that the first task must start before the second task
         starts.
         """
-        self._add_constaint(task1, task2, Constraint.START_BEFORE_START)
+        self._add_constraint(task1, task2, Constraint.START_BEFORE_START)
 
     def add_start_before_end(self, task1: Task, task2: Task):
         """
         Adds a constraint that the first task must start before the second task
         ends.
         """
-        self._add_constaint(task1, task2, Constraint.START_BEFORE_END)
+        self._add_constraint(task1, task2, Constraint.START_BEFORE_END)
 
     def add_end_at_end(self, task1: Task, task2: Task):
         """
         Adds a constraint that the first task must end at the same time as the
         second task ends.
         """
-        self._add_constaint(task1, task2, Constraint.END_AT_END)
+        self._add_constraint(task1, task2, Constraint.END_AT_END)
 
     def add_end_at_start(self, task1: Task, task2: Task):
         """
         Adds a constraint that the first task must end at the same time as the
         second task starts.
         """
-        self._add_constaint(task1, task2, Constraint.END_AT_START)
+        self._add_constraint(task1, task2, Constraint.END_AT_START)
 
     def add_end_before_start(self, task1: Task, task2: Task):
         """
         Adds a constraint that the first task must end before the second task
         starts.
         """
-        self._add_constaint(task1, task2, Constraint.END_BEFORE_START)
+        self._add_constraint(task1, task2, Constraint.END_BEFORE_START)
 
     def add_end_before_end(self, task1: Task, task2: Task):
         """
         Adds a constraint that the first task must end before the second task
         ends.
         """
-        self._add_constaint(task1, task2, Constraint.END_BEFORE_END)
+        self._add_constraint(task1, task2, Constraint.END_BEFORE_END)
 
     def add_identical_resources(self, task1: Task, task2: Task):
         """
         Adds a constraint that two tasks must be scheduled with modes that
         require identical resources.
         """
-        self._add_constaint(task1, task2, Constraint.IDENTICAL_RESOURCES)
+        self._add_constraint(task1, task2, Constraint.IDENTICAL_RESOURCES)
 
     def add_different_resource(self, task1: Task, task2: Task):
         """
         Adds a constraint that the two tasks must be scheduled with modes that
         require different resources.
         """
-        self._add_constaint(task1, task2, Constraint.DIFFERENT_RESOURCES)
+        self._add_constraint(task1, task2, Constraint.DIFFERENT_RESOURCES)
 
     def add_consecutive(self, task1: Task, task2: Task):
         """
@@ -464,7 +466,7 @@ class Model:
         the second task, meaning that no task is allowed to schedule between,
         on machines that they are both scheduled on.
         """
-        self._add_constaint(task1, task2, Constraint.CONSECUTIVE)
+        self._add_constraint(task1, task2, Constraint.CONSECUTIVE)
 
     def add_setup_time(
         self, machine: Machine, task1: Task, task2: Task, duration: int
