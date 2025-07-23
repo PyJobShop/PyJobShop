5355219e4ab77c3a20bfed3b2d68b9aeb1d3bc58
Leon Lan <l.lan@vu.nl>
2025-07-23 11:58:54 +0200

    Warmstart present

--- pyjobshop/solvers/cpoptimizer/Variables.py
+++ pyjobshop/solvers/cpoptimizer/Variables.py
@@ -163,11 +163,12 @@ class Variables:
             job_var = self.job_vars[idx]
             sol_tasks = [solution.tasks[task] for task in job.tasks]
 
+            present = any(task.present for task in sol_tasks)
             job_start = min(task.start for task in sol_tasks if task.present)
             job_end = max(task.end for task in sol_tasks if task.present)
 
             init.add_interval_var_solution(
-                job_var, start=job_start, end=job_end
+                job_var, presence=present, start=job_start, end=job_end
             )
 
         for idx in range(data.num_tasks):
