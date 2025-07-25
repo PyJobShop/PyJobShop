7ee12511d06d5468a78d8cab51a599a0faef6425
Leon Lan <l.lan@vu.nl>
2025-07-25 14:03:06 +0200

    Small changes

--- pyjobshop/ProblemData.py
+++ pyjobshop/ProblemData.py
@@ -543,8 +543,8 @@ class SetupTime(IterableMixin):
 @dataclass
 class ModeDependency(IterableMixin):
     """
-    Represents a dependency between task modes: if `mode1` is selected,
-    then at least one of the modes in `modes2` must also be selected.
+    Represents a dependency between task modes: if mode 1 is selected,
+    then at least one of the modes in modes 2 must also be selected.
 
     Let :math:`m_1` be the Boolean variable indicating whether `mode1` is
     selected. Let :math:`M_2` be the set of Boolean variables corresponding
