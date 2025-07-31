ff197813ba77a7bfa61e6c4a65a633331addce8d
Leon Lan <l.lan@vu.nl>
2025-07-31 16:56:55 +0200

    Lazily create all other variables, too

--- pyjobshop/solvers/ortools/Objective.py
+++ pyjobshop/solvers/ortools/Objective.py
@@ -5,7 +5,6 @@ from ortools.sat.python.cp_model import (
 )
 
 import pyjobshop.solvers.utils as utils
-from pyjobshop.constants import MAX_VALUE
 from pyjobshop.ProblemData import Objective as ObjectiveData
 from pyjobshop.ProblemData import ProblemData
 
@@ -24,157 +23,86 @@ class Objective:
         self._data = data
         self._variables = variables
 
-    def _makespan_expr(self) -> LinearExprT:
-        """
-        Returns an expression representing the makespan of the model.
-        """
-        return self._variables.makespan_var
-
-    def _tardy_jobs_expr(self) -> LinearExprT:
-        """
-        Returns an expression representing the number of tardy jobs.
-        """
-        model, data = self._model, self._data
-        is_tardy_vars = []
-
-        for job, job_var in zip(data.jobs, self._variables.job_vars):
-            assert job.due_date is not None
-            is_tardy = model.new_bool_var(f"is_tardy_{job}")
-            model.add(job_var.end > job.due_date).only_enforce_if(is_tardy)
-            model.add(job_var.end <= job.due_date).only_enforce_if(~is_tardy)
-            is_tardy_vars.append(is_tardy)
-
-        weights = [job.weight for job in data.jobs]
-        return LinearExpr.weighted_sum(is_tardy_vars, weights)
-
-    def _total_flow_time_expr(self) -> LinearExprT:
-        """
-        Returns an expression representing the total flow time of jobs.
-        """
-        model, data = self._model, self._data
-        flow_time_vars = []
-
-        for job, var in zip(data.jobs, self._variables.job_vars):
-            flow_time = model.new_int_var(0, MAX_VALUE, f"flow_time_{job}")
-            model.add_max_equality(flow_time, [0, var.end - job.release_date])
-            flow_time_vars.append(flow_time)
-
-        weights = [job.weight for job in data.jobs]
-        return LinearExpr.weighted_sum(flow_time_vars, weights)
-
-    def _total_tardiness_expr(self) -> LinearExprT:
-        """
-        Returns an expression representing the total tardiness of jobs.
-        """
-        model, data = self._model, self._data
-        tardiness_vars = []
-
-        for job, var in zip(data.jobs, self._variables.job_vars):
-            assert job.due_date is not None
-            tardiness = model.new_int_var(0, MAX_VALUE, f"tardiness_{job}")
-            model.add_max_equality(tardiness, [0, var.end - job.due_date])
-            tardiness_vars.append(tardiness)
-
-        weights = [job.weight for job in data.jobs]
-        return LinearExpr.weighted_sum(tardiness_vars, weights)
-
-    def _total_earliness_expr(self) -> LinearExprT:
-        """
-        Returns an expression representing the total earliness of jobs.
-        """
-        model, data = self._model, self._data
-        earliness_vars = []
-
-        for job, var in zip(data.jobs, self._variables.job_vars):
-            assert job.due_date is not None
-            earliness = model.new_int_var(0, MAX_VALUE, f"earliness_{job}")
-            model.add_max_equality(earliness, [0, job.due_date - var.end])
-            earliness_vars.append(earliness)
-
-        weights = [job.weight for job in data.jobs]
-        return LinearExpr.weighted_sum(earliness_vars, weights)
-
-    def _max_tardiness_expr(self) -> LinearExprT:
-        """
-        Returns an expression representing the maximum tardiness of jobs.
-        """
-        model, data = self._model, self._data
-        tardiness_vars = []
-
-        for job, var in zip(data.jobs, self._variables.job_vars):
-            assert job.due_date is not None
-            tardiness = model.new_int_var(0, MAX_VALUE, f"tardiness_{job}")
-            model.add_max_equality(tardiness, [0, var.end - job.due_date])
-            tardiness_vars.append(job.weight * tardiness)
-
-        max_tardiness = model.new_int_var(0, MAX_VALUE, "max_tardiness")
-        model.add_max_equality(max_tardiness, tardiness_vars)
-        return max_tardiness
-
-    def _max_lateness_expr(self) -> LinearExprT:
-        """
-        Returns an expression representing the maximum tardiness of jobs.
-        """
-        model, data = self._model, self._data
-        lateness_vars = []
-
-        for job, var in zip(data.jobs, self._variables.job_vars):
-            assert job.due_date is not None
-            lateness = model.new_int_var(
-                -MAX_VALUE, MAX_VALUE, f"lateness_{job}"
-            )
-            model.add(lateness == var.end - job.due_date)
-            lateness_vars.append(job.weight * lateness)
-
-        max_lateness = model.new_int_var(-MAX_VALUE, MAX_VALUE, "max_lateness")
-        model.add_max_equality(max_lateness, lateness_vars)
-        return max_lateness
-
-    def _total_setup_time_expr(self) -> LinearExprT:
-        """
-        Returns an expression representing the total setup time of tasks.
-        """
-        data, variables = self._data, self._variables
-        setup_times = utils.setup_times_matrix(data)
-
-        setup_time_vars = []
-        for res_idx in data.machine_idcs:
-            seq_var = variables.sequence_vars[res_idx]
-            if not seq_var.is_active:
-                continue
-
-            for task_idx1 in range(data.num_tasks):
-                for task_idx2 in range(data.num_tasks):
-                    var1 = variables.assign_vars.get((task_idx1, res_idx))
-                    var2 = variables.assign_vars.get((task_idx2, res_idx))
-                    if not (var1 and var2):
-                        continue
-
-                    setup = (
-                        setup_times[res_idx, task_idx1, task_idx2]
-                        if setup_times is not None
-                        else 0
-                    )
-                    arc_selected = seq_var.arcs[task_idx1, task_idx2]
-                    setup_time_vars.append(arc_selected * setup)
-
-        return LinearExpr.sum(setup_time_vars)
-
     def _objective_expr(self, objective: ObjectiveData) -> LinearExprT:
         """
         Returns the expression corresponding to the given objective.
         """
-        items = [
-            (objective.weight_makespan, self._makespan_expr),
-            (objective.weight_tardy_jobs, self._tardy_jobs_expr),
-            (objective.weight_total_tardiness, self._total_tardiness_expr),
-            (objective.weight_total_flow_time, self._total_flow_time_expr),
-            (objective.weight_total_earliness, self._total_earliness_expr),
-            (objective.weight_max_tardiness, self._max_tardiness_expr),
-            (objective.weight_max_lateness, self._max_lateness_expr),
-            (objective.weight_total_setup_time, self._total_setup_time_expr),
-        ]
-        exprs = [weight * expr() for weight, expr in items if weight > 0]
+        variables = self._variables
+        job_weights = [job.weight for job in self._data.jobs]
+        exprs = []
+
+        if objective.weight_makespan > 0:
+            exprs += [objective.weight_makespan * variables.makespan_var]
+
+        if objective.weight_tardy_jobs > 0:
+            exprs += [
+                objective.weight_tardy_jobs
+                * LinearExpr.weighted_sum(variables.is_tardy_vars, job_weights)
+            ]
+
+        if objective.weight_total_flow_time > 0:
+            exprs += [
+                objective.weight_total_flow_time
+                * LinearExpr.weighted_sum(
+                    variables.flow_time_vars, job_weights
+                )
+            ]
+
+        if objective.weight_total_tardiness > 0:
+            exprs += [
+                objective.weight_total_tardiness
+                * LinearExpr.weighted_sum(
+                    variables.tardiness_vars, job_weights
+                )
+            ]
+
+        if objective.weight_total_earliness > 0:
+            exprs += [
+                objective.weight_total_earliness
+                * LinearExpr.weighted_sum(
+                    variables.earliness_vars, job_weights
+                )
+            ]
+
+        if objective.weight_max_tardiness > 0:
+            exprs += [
+                objective.weight_max_tardiness * variables.max_tardiness_var
+            ]
+
+        if objective.weight_max_lateness > 0:
+            exprs += [
+                objective.weight_max_lateness * variables.max_lateness_var
+            ]
+
+        if objective.weight_total_setup_time > 0:
+            data = self._data
+            setup_times = utils.setup_times_matrix(data)
+            setup_time_vars = []
+            for res_idx in data.machine_idcs:
+                seq_var = variables.sequence_vars[res_idx]
+                if not seq_var.is_active:
+                    continue
+
+                for task_idx1 in range(data.num_tasks):
+                    for task_idx2 in range(data.num_tasks):
+                        var1 = variables.assign_vars.get((task_idx1, res_idx))
+                        var2 = variables.assign_vars.get((task_idx2, res_idx))
+                        if not (var1 and var2):
+                            continue
+
+                        setup = (
+                            setup_times[res_idx, task_idx1, task_idx2]
+                            if setup_times is not None
+                            else 0
+                        )
+                        arc_selected = seq_var.arcs[task_idx1, task_idx2]
+                        setup_time_vars.append(arc_selected * setup)
+
+            exprs += [
+                objective.weight_total_setup_time
+                * LinearExpr.sum(setup_time_vars)
+            ]
+
         return LinearExpr.sum(exprs)
 
     def add_objective(self):
--- pyjobshop/solvers/ortools/Variables.py
+++ pyjobshop/solvers/ortools/Variables.py
@@ -163,7 +163,14 @@ class Variables:
         self._assign_vars = self._make_assign_variables(self._task_vars)
         self._sequence_vars = self._make_sequence_variables()
 
-        self._makespan_var = None
+        # Variables below are lazily created, only when relevant.
+        self._makespan_var: IntVar | None = None
+        self._is_tardy_vars: list[IntVar] | None = None
+        self._flow_time_vars: list[IntVar] | None = None
+        self._tardiness_vars: list[IntVar] | None = None
+        self._earliness_vars: list[IntVar] | None = None
+        self._max_tardiness_var: IntVar | None = None
+        self._max_lateness_var: IntVar | None = None
 
     @property
     def job_vars(self) -> list[JobVar]:
@@ -216,6 +223,136 @@ class Variables:
             self._model.add_max_equality(self._makespan_var, completion_times)
 
         return self._makespan_var
+
+    @property
+    def is_tardy_vars(self) -> list[IntVar]:
+        """
+        Returns the Boolean variables indicating whether each job is tardy,
+        creating them if they do not exist.
+        """
+        if self._is_tardy_vars is not None:
+            return self._is_tardy_vars
+
+        model, data = self._model, self._data
+        self._is_tardy_vars = []
+
+        for job, job_var in zip(data.jobs, self._job_vars):
+            assert job.due_date is not None
+            is_tardy = model.new_bool_var(f"is_tardy_{job}")
+            model.add(job_var.end > job.due_date).only_enforce_if(is_tardy)
+            model.add(job_var.end <= job.due_date).only_enforce_if(~is_tardy)
+            self._is_tardy_vars.append(is_tardy)
+
+        return self._is_tardy_vars
+
+    @property
+    def flow_time_vars(self) -> list[IntVar]:
+        """
+        Returns the flow time variables for each job, creating them if they do
+        not exist.
+        """
+        if self._flow_time_vars is not None:
+            return self._flow_time_vars
+
+        model, data = self._model, self._data
+        self._flow_time_vars = []
+
+        for job, var in zip(data.jobs, self._job_vars):
+            flow_time = model.new_int_var(0, MAX_VALUE, f"flow_time_{job}")
+            model.add_max_equality(flow_time, [0, var.end - job.release_date])
+            self._flow_time_vars.append(flow_time)
+
+        return self._flow_time_vars
+
+    @property
+    def tardiness_vars(self) -> list[IntVar]:
+        """
+        Returns the tardiness variables for each job, creating them if they do
+        not exist.
+        """
+        if self._tardiness_vars is not None:
+            return self._tardiness_vars
+
+        model, data = self._model, self._data
+        self._tardiness_vars = []
+
+        for job, var in zip(data.jobs, self._job_vars):
+            assert job.due_date is not None
+            tardiness = model.new_int_var(0, MAX_VALUE, f"tardiness_{job}")
+            model.add_max_equality(tardiness, [0, var.end - job.due_date])
+            self._tardiness_vars.append(tardiness)
+
+        return self._tardiness_vars
+
+    @property
+    def earliness_vars(self) -> list[IntVar]:
+        """
+        Returns the earliness variables for each job, creating them if they do
+        not exist.
+        """
+        if self._earliness_vars is not None:
+            return self._earliness_vars
+
+        model, data = self._model, self._data
+        self._earliness_vars = []
+
+        for job, var in zip(data.jobs, self._job_vars):
+            assert job.due_date is not None
+            earliness = model.new_int_var(0, MAX_VALUE, f"earliness_{job}")
+            model.add_max_equality(earliness, [0, job.due_date - var.end])
+            self._earliness_vars.append(earliness)
+
+        return self._earliness_vars
+
+    @property
+    def max_tardiness_var(self) -> IntVar:
+        """
+        Returns the maximum tardiness variable, creating it if it does not
+        exist.
+        """
+        if self._max_tardiness_var is not None:
+            return self._max_tardiness_var
+
+        model, data = self._model, self._data
+        tardiness_vars = []
+
+        for job, var in zip(data.jobs, self._job_vars):
+            assert job.due_date is not None
+            tardiness = model.new_int_var(0, MAX_VALUE, f"tardiness_{job}")
+            model.add_max_equality(tardiness, [0, var.end - job.due_date])
+            tardiness_vars.append(job.weight * tardiness)
+
+        self._max_tardiness_var = model.new_int_var(
+            0, MAX_VALUE, "max_tardiness"
+        )
+        model.add_max_equality(self._max_tardiness_var, tardiness_vars)
+        return self._max_tardiness_var
+
+    @property
+    def max_lateness_var(self) -> IntVar:
+        """
+        Returns the maximum lateness variable, creating it if it does not
+        exist.
+        """
+        if self._max_lateness_var is not None:
+            return self._max_lateness_var
+
+        model, data = self._model, self._data
+        lateness_vars = []
+
+        for job, var in zip(data.jobs, self._job_vars):
+            assert job.due_date is not None
+            lateness = model.new_int_var(
+                -MAX_VALUE, MAX_VALUE, f"lateness_{job}"
+            )
+            model.add(lateness == var.end - job.due_date)
+            lateness_vars.append(job.weight * lateness)
+
+        self._max_lateness_var = model.new_int_var(
+            -MAX_VALUE, MAX_VALUE, "max_lateness"
+        )
+        model.add_max_equality(self._max_lateness_var, lateness_vars)
+        return self._max_lateness_var
 
     def res2assign(self, idx: int) -> list[AssignVar]:
         """
@@ -400,6 +537,7 @@ class Variables:
         if self._data.objective.weight_makespan > 0:
             model.add_hint(self.makespan_var, solution.makespan)
 
+        # TODO only add hints if they are relevant.
         # items = [
         #     objective.weight_makespan,
         #     objective.weight_tardy_jobs,
