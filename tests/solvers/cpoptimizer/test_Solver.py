62ee31cc9ae5a7facfccee318d1f64606d8ac216
Leon Lan <30997278+leonlan@users.noreply.github.com>
2025-08-11 09:49:36 +0200

    Upgrade workflows (#347)

--- tests/solvers/cpoptimizer/test_Solver.py
+++ tests/solvers/cpoptimizer/test_Solver.py
@@ -1,13 +1,18 @@
 from numpy.testing import assert_
 
-from pyjobshop.solvers.cpoptimizer.Solver import Solver
 
-
-def test_solve_initial_solution(complete_data, complete_sol, capfd):
+def test_solve_initial_solution(
+    require_cpoptimizer,
+    complete_data,
+    complete_sol,
+    capfd,
+):
     """
     Tests that the solver correctly hints the solution by checking that the
     display log is correct when an initial solution is provided.
     """
+    from pyjobshop.solvers.cpoptimizer.Solver import Solver
+
     solver = Solver(complete_data)
     solver.solve(display=True, initial_solution=complete_sol)
 
